e1ac8955d8948e7912c2febcac9a8050e0da6e19
omersahin <1sahinomer1@gmail.com>
2021-05-07 22:19:05 +0300

    improvements

--- src/models/user/user.ts
+++ src/models/user/user.ts
@@ -1,3 +1,27 @@
+import  { Room }  from "../room/room";
+
+
 export class User {
+    id: string;
+    name: string;
+    room: Room=null;
+
+    constructor(id:string,name:string){
+        this.id=id;
+        this.name=name;
+        this.room;
+        User.allUsers.push(this);
+    }
+
+    static allUsers : User[] = [];
+    static all(){
+        return this.allUsers
+    }
+    static get(id:string){
+        return this.allUsers.find((e)=>e.id===id)
+    }
+    getRoom(){
+        return this.room;
+    }
 
 }
